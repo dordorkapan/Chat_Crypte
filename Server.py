417542872ceefc56cfa34b5f9b564af7bc1d09f7
Dora <90462122+dordorkapan@users.noreply.github.com>
2022-04-08 15:52:58 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -4,11 +4,25 @@ import threading
 host = '127.0.0.1'
 port = 55555
 
-# Starting Server
+# Serveur de démarrage
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
 
-# Lists For Clients and Their Nicknames
+# Listes de clients et leurs Nicknames
 clients = []
 nicknames = []
+
+
+def broadcast(message):
+  """
+  Où nous envoyons le message à tous les clients
+  """
+def gerer(client):
+  """
+  Gérer les clients qui sont entrain d'arriver
+  """
+def recevoir():
+  """
+  Reçoit des clients et ses messages
+  """
