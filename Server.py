6b089fa690827de5f2bc1aee1acf5cb6deef1e5d
Dora <90462122+dordorkapan@users.noreply.github.com>
2022-04-08 15:34:46 +0200

    Create Server.py

---
+++ Server.py
@@ -0,0 +1,14 @@
+import socket
+import threading
+
+host = '127.0.0.1'
+port = 55555
+
+# Starting Server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+# Lists For Clients and Their Nicknames
+clients = []
+nicknames = []
