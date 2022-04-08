d04f2c4c8c839aa217c9be772e8f37e3bc00dbfa
Dora <90462122+dordorkapan@users.noreply.github.com>
2022-04-08 15:53:06 +0200

    Create Client.py

---
+++ Client.py
@@ -0,0 +1,23 @@
+import socket
+import threading
+
+# Choisir le Nickname
+nickname = input("Choisissez votre pseudo: ")
+
+# Connexion au Serveur
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
+
+class Client():
+  
+  def _init_(self, host, port):
+  
+  def gui_loop(self):
+  
+  def ecrire(self):
+  
+  def cripter(self, message):
+    
+  def arreter(self):
+  
+  def recevoir(self):
