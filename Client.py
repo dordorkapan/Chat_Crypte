b6b13cc086b5bb967b0ca3193a6e970f4b00455a
Dora <90462122+dordorkapan@users.noreply.github.com>
2022-04-11 09:58:43 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -3,19 +3,25 @@ import sys
 from _thread import *
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# vérifie si suffisamment d'arguments ont été fournis
 if len(sys.argv) != 3:
-	print ("Utilisation correcte : script, adresse IP, numéro de port")
-	exit()
+    IP_address = "172.20.12.20"
+    Port = 2222
+else:
+    # prend le premier argument de l'invite de commande comme adresse IP
+    IP_address = str(sys.argv[1])
+    
+    # prend le deuxième argument de l'invite de commande comme numéro de port
+    Port = int(sys.argv[2])
 
-IP_address = str(sys.argv[1])
-Port = int(sys.argv[2])
-server.connect((IP_address, Port))
+#lie le serveur à une adresse IP saisie et à la numéro de port spécifié.
+server.bind((IP_address, Port))
 
 
 def send_msg(sock):
-"""
-Le programme reçoit un message que vous écrivez, l'envoie à l'autre client et l'affiche sur votre écran
-"""
+    """
+    Le programme reçoit un message que vous écrivez, l'envoie à l'autre client et l'affiche sur votre écran
+    """
     while True:
         data = sys.stdin.readline()
         #encrypt 
