87607829780b218d6643e9860211f077d14360e7
Dora <90462122+dordorkapan@users.noreply.github.com>
2022-04-13 11:36:44 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 from _thread import *
+from simplecrypt import encrypt,decrypt
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # vérifie si suffisamment d'arguments ont été fournis
@@ -29,14 +30,17 @@ Port = int(sys.argv[2])
 #lie le serveur à une adresse IP saisie et à la numéro de port spécifié.
 server.bind((IP_address, Port))
 
+name = input("Your name : ") # name to identify your self
+passwd = getpass.getpass("Enter password set by client for encrypted chat : ") # password to encrypt chat msgs
+
 def send_msg(sock):
     """
     Le programme reçoit un message que vous écrivez, l'envoie à l'autre client et l'affiche sur votre écran
     """
     while True:
         data = sys.stdin.readline()
-        #encrypt 
-        sock.send(data.encode())
+        encMsg = encrypt(passwd,(name + " : " + data)) # encrypting msg
+        sock.send(encMsg.encode())
         sys.stdout.write("<You>")
         sys.stdout.write(str(data))
         sys.stdout.flush()
@@ -46,7 +50,7 @@ start_new_thread(send_msg,(server,))
 
 while True:
     message = server.recv(2048)
-    #decrypt
+    print(decrypt(passwd,server.recv(1024)).decode('utf-8'))
     print (message)
 
 
